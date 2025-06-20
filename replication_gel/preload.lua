93595ddb4a7449f630dc4046ed6b95495f12e2ef
shmakota <47482311+shmakota@users.noreply.github.com>
2025-06-19 23:52:25 -0500

    lua fix

--- replication_gel/preload.lua
+++ replication_gel/preload.lua
@@ -3,6 +3,6 @@
 local mod = game.mod_runtime[game.current_mod]
 
 -- Register our item use function
-game.iuse_functions["DUPLICATION_GEL"] = function(...)
+game.iuse_functions["REPLICATION_GEL"] = function(...)
   return mod.iuse_function(...)
 end
